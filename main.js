a4467092239cb300865376303afe8aa33516890f
juanchimdo <64117883+juanchimdo@users.noreply.github.com>
2020-06-05 12:31:22 -0300

    maquetado

---
+++ main.js
@@ -0,0 +1,12 @@
+window.onload = function () {
+    const silla1 = document.createElement ('img')
+    const silla2 = document.createElement ('img')
+
+    const wrap = this.document.querySelector('.wrap')
+
+
+    silla1.src = "silla.png"
+
+
+    silla2.src = "silla.png"
+}
