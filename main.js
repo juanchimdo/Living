c37dae2387bc9a4dba1255b24d51e74745cc164a
juanchimdo <64117883+juanchimdo@users.noreply.github.com>
2020-06-05 16:07:38 -0300

    primer intento

--- main.js
+++ main.js
@@ -1,12 +1,52 @@
 window.onload = function () {
-    const silla1 = document.createElement ('img')
-    const silla2 = document.createElement ('img')
 
-    const wrap = this.document.querySelector('.wrap')
+    const wrap = document.querySelector('.wrap')
 
 
-    silla1.src = "silla.png"
+
+    const arriba = document.querySelector('.arriba')
+    const centro = document.querySelector('.centro')
+    const abajo = document.querySelector('.abajo')
+    const izquierda = document.querySelector('.izquierda')
+    const derecha = document.querySelector('.derecha')
 
 
-    silla2.src = "silla.png"
+    const mas = document.querySelector('.mas')
+    const menos = document.querySelector('.menos')
+
+    function agregarSilla () {
+        contador = document.querySelectorAll('.silla').length
+        if (contador < 3) {
+            const silla = document.createElement ('img')
+            silla.src = "silla.png"
+            silla.classList.add('silla')
+            arriba.append(silla)
+        }else if (contador < 4) {
+            const silla = document.createElement ('img')
+            silla.src = "silla.png"
+            silla.classList.add('silla', 'izquierda')
+            izquierda.append(silla)
+        }else if (contador < 5) {
+            const silla = document.createElement ('img')
+            silla.src = "silla.png"
+            silla.classList.add('silla', 'derecha')
+            derecha.append(silla)
+        }else if (contador < 8) {
+            const silla = document.createElement ('img')
+            silla.src = "silla.png"
+            silla.classList.add('silla')
+            abajo.append(silla)
+        }
+        console.log(contador)
+    }
+
+    function quitarSilla () {
+        const imagenes = document.querySelectorAll('.silla')
+        imagenes[imagenes.length-1].remove()
+        console.log(contador)
+    }
+
+
+    mas.addEventListener('click', agregarSilla)
+    menos.addEventListener('click', quitarSilla)
 }
