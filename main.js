e4dbdfde517f2ed84c878f1522494c8839777134
juanchimdo <64117883+juanchimdo@users.noreply.github.com>
2020-06-06 14:11:11 -0300

    funciona con random

--- main.js
+++ main.js
@@ -1,48 +1,30 @@
 window.onload = function () {
-    const arriba = document.querySelector('.arriba')
-    const abajo = document.querySelector('.abajo')
-    const izquierda = document.querySelector('.izquierda')
-    const derecha = document.querySelector('.derecha')
     const mas = document.querySelector('.mas')
     const menos = document.querySelector('.menos')
 
-    let aleatorio = Math.floor((Math.random() * 8) + 1)
 
     function agregarSilla () {
-        const sillasActuales = document.querySelectorAll('.silla').length
-        let aleatorio = Math.floor((Math.random() * 8) + sillasActuales)
-        if (contador < 3) { 
-            nuevaSilla(arriba)
-        }else if (contador < 4) {
-            nuevaSilla(izquierda, 'izquierda')
-        }else if (contador < 5) {
-            nuevaSilla(derecha, 'derecha')
-        }else if (contador < 8) {
-            nuevaSilla(abajo)
+        if (document.querySelectorAll('.visible').length<8) {
+            const sillasActuales = document.querySelectorAll('.visible').length
+            const ocultas = 8 - sillasActuales
+            const random = Math.floor((Math.random() * ocultas) + 1)-1
+            const silla = document.querySelectorAll('.oculto')[random]
+            silla.classList.remove('oculto')
+            silla.classList.add('visible')
         }
     }
+
     function quitarSilla () {
-        const imagenes = document.querySelectorAll('.silla')
-        imagenes[imagenes.length-1].remove()
+        if (document.querySelectorAll('.visible').length>0) {
+            const sillasActuales = document.querySelectorAll('.visible').length
+            const random = Math.floor((Math.random() * sillasActuales) + 1)-1
+            const silla = document.querySelectorAll('.visible')[random]
+            silla.classList.add('oculto')
+            silla.classList.remove('visible')
+        }
     }
+
+
     mas.addEventListener('click', agregarSilla)
     menos.addEventListener('click', quitarSilla)
 }
-
-
-
-
-
-
-function nuevaSilla (lugar, clase) {
-    const silla = document.createElement ('img')
-    silla.src = "silla.png"
-    silla.classList.add('silla', clase)
-    lugar.append(silla)
-}
-
-
-// 1=arriba
-// 2=derecha
-// 3=izquierda
-// 4=abajo
