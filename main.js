45db533e5e6003a580f154f280c2b8ee16688c44
juanchimdo <64117883+juanchimdo@users.noreply.github.com>
2020-06-09 12:26:56 -0300

    todo en una funcion

--- main.js
+++ main.js
@@ -1,27 +1,24 @@
 window.onload = function () {
     const mas = document.querySelector('.mas')
     const menos = document.querySelector('.menos')
-
     function agregarSilla () {
-        const ocultas = document.querySelectorAll('.oculto').length
-        if (ocultas>0) {
-            const random = Math.floor((Math.random() * ocultas) + 1)-1
-            const silla = document.querySelectorAll('.oculto')[random]
-            silla.classList.remove('oculto')
-            silla.classList.add('visible')
-        }
+        cambiarClase('oculto','visible')
     }
-
     function quitarSilla () {
-        const visibles = document.querySelectorAll('.visible').length
-        if (visibles>0) {
-            const random = Math.floor((Math.random() * visibles) + 1)-1
-            const silla = document.querySelectorAll('.visible')[random]
-            silla.classList.add('oculto')
-            silla.classList.remove('visible')
-        }
+        cambiarClase('visible','oculto')
     }
-
     mas.addEventListener('click', agregarSilla)
     menos.addEventListener('click', quitarSilla)
+}
+
+
+
+function cambiarClase (claseActual, clasePosterior) {
+    const cantidad = document.querySelectorAll('.' + claseActual).length
+    if (cantidad>0) {
+        const random = Math.floor((Math.random() * cantidad) + 1)-1
+        const silla = document.querySelectorAll('.' + claseActual)[random]
+        silla.classList.remove(claseActual)
+        silla.classList.add(clasePosterior)
+    }
 }
