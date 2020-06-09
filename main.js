7af68dadcfee0297d389518f7616720f5241455a
juanchimdo <64117883+juanchimdo@users.noreply.github.com>
2020-06-09 16:47:51 -0300

    explicaciones

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 window.onload = function () {
-    const mas = document.querySelector('.mas')
+    const mas = document.querySelector('.mas')    
     const menos = document.querySelector('.menos')
     function agregarSilla () {
         cambiarClase('oculto','visible')
@@ -14,11 +14,11 @@ window.onload = function () {
 
 
 function cambiarClase (claseActual, clasePosterior) {
-    const cantidad = document.querySelectorAll('.' + claseActual).length
-    if (cantidad>0) {
-        const random = Math.floor((Math.random() * cantidad) + 1)-1
-        const silla = document.querySelectorAll('.' + claseActual)[random]
-        silla.classList.remove(claseActual)
-        silla.classList.add(clasePosterior)
+    const cantidad = document.querySelectorAll('.' + claseActual).length        //CANTIDAD DE SILLAS OCULTAS O VISIBLES DEPENDIENDO EL PARAMETRO
+    if (cantidad>0) {                                                           //ME ASEGURO QUE NO VOY A AGREGAR/QUITAR MAS DE LO POSIBLE
+        const random = Math.floor((Math.random() * cantidad) + 1)-1             //GENERO UN NUMERO RANDOM ENTRE 1 Y LA CANTIDAD DE SILLAS ACTUALES (VISIBLE U OCULTA, DEPENDIENDO LA FUNCION QUE SE QUIERA). LE RESTO -1 PARA USARLO COMO INDICE
+        const silla = document.querySelectorAll('.' + claseActual)[random]      //GUARDO EN LA VARIABLE 'SILLA' EL ELEMENTO EN LA POSICION 'RANDOM'
+        silla.classList.remove(claseActual)                                     //LE QUITO LA CLASE ACTUAL
+        silla.classList.add(clasePosterior)                                     //AGREGO LA NUEVA CLASE
     }
 }
